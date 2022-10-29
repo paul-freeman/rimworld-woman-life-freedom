b67d0d9846396b3dc034bd1585b1038223163c73
Paul Freeman <paul.freeman.cs@gmail.com>
2022-10-29 14:20:13 +1300

    feat: women with head coverings get a mood penalty

---
+++ WomenLifeFreedom.cs
@@ -0,0 +1,14 @@
+using RimWorld;
+using Verse;
+
+namespace WomenLifeFreedom
+{
+    [StaticConstructorOnStartup]
+    public static class WomenLifeFreedom_Mod
+    {
+        static WomenLifeFreedom_Mod()
+        {
+            Log.Warning("Thank you for supporting WomenLifeFreedom! #MahsaAmini");
+        }
+    }
+}
