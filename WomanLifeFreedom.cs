3637a2ff1302f9e1d4a488898ee180703269ac3b
Paul Freeman <paul.freeman.cs@gmail.com>
2022-10-29 18:12:16 +1300

    refactor: rename women to woman (#4)

---
+++ WomanLifeFreedom.cs
@@ -0,0 +1,14 @@
+using RimWorld;
+using Verse;
+
+namespace WomanLifeFreedom
+{
+    [StaticConstructorOnStartup]
+    public static class WomanLifeFreedom_Mod
+    {
+        static WomanLifeFreedom_Mod()
+        {
+            Log.Warning("Thank you for supporting WomanLifeFreedom! #MahsaAmini");
+        }
+    }
+}
